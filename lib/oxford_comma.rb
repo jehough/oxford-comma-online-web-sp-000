9004e7ae8faf347dfa6e1730faa3c5de9c628435
Jim Hough <teachlife@outlook.com>
2019-04-22 00:25:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,12 @@
 def oxford_comma(array)
-  last_word = array[-1]
-  array [-1] = 'and '
-  comma_list = array.join (', ')
-  return comma_list << last_word
+  if array.length == 1 
+    return array.join
+  elsif array.length == 2
+    return array.join (' and ')
+  else
+    last_word = array[-1]
+    array [-1] = 'and '
+    comma_list = array.join (', ')
+    return comma_list << last_word
+  end
 end
